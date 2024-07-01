8a8ae4201dee7599c425540f190901a2f0612f70
Hannah May Tabunan <hannah.tabunan@gmail.com>
2024-07-01 22:52:13 +0800

    Update the script and remove JQuery

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-$(document).ready(
-    function() {
-        $('.hamburger').click(function() {
-            $('.hamburger').toggleClass('active');
-            $('.nav-list ul').toggleClass('active');
-        })
-    }
-)
+const hamburger = document.querySelector('.header .hamburger');
+const mobileMenu = document.querySelectorAll('.header .nav-list ul')[0];
+const header = document.querySelector('#header');
+const headerLogo = document.querySelector('#header .logo');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobileMenu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 300) {
+		header.style.backgroundColor = '#1C0221';
+        headerLogo.style.visibility = 'visible';
+	} else {
+		header.style.backgroundColor = 'transparent';
+        headerLogo.style.visibility = 'hidden';
+	}
+});
